4563fc02db5da39de8045ea48bc616d7cb935f5a
unknown <rockstro@uni-potsdam.de>
2023-07-13 09:25:53 +0200

    added file for styling with css

---
+++ style.py
@@ -0,0 +1,100 @@
+import streamlit as st
+
+# hide hamburger menu
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        #main-header {display: none};
+        </style>
+        """
+custom_style = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background: hsla(0, 0%, 100%, 1);
+
+background: linear-gradient(45deg, hsla(0, 0%, 100%, 1) 70%, hsla(326, 52%, 44%, 1) 100%);
+
+background: -moz-linear-gradient(45deg, hsla(0, 0%, 100%, 1) 70%, hsla(326, 52%, 44%, 1) 100%);
+
+background: -webkit-linear-gradient(45deg, hsla(0, 0%, 100%, 1) 70%, hsla(326, 52%, 44%, 1) 100%);
+
+filter: progid: DXImageTransform.Microsoft.gradient( startColorstr="#FFFFFF", endColorstr="#AA3678", GradientType=1 );
+}}
+
+div[data-testid="stSidebar"] {{display: none;}}
+
+[data-testid="stHeader"] {{
+background: #25be8e;
+}}
+
+[data-baseweb="stAlert"] {{
+background: #F5F5F5;
+}}
+
+[data-testid="stExpander"] {{
+background: #F5F5F5;
+}}
+
+[data-testid="stSelectbox"] {{
+color: #F8F8FF;
+}}
+
+[data-testid="stToolbar"] {{
+right: 2rem;
+}}
+
+[data-testid="stHeader"] {{
+    display: none !important;
+}}
+</style>
+"""
+css_info = f"""
+<style>
+    .info-box {{
+        background-color: #F5F5F5; /* Replace with your desired background color */
+        padding: 1rem;
+        border-radius: 5px;
+    }}
+</style>
+"""
+footer = """
+        <style>
+            .footer {
+                position: fixed;
+                left: 0;
+                bottom: 0;
+                width: 100%;
+                background-color: #f
+                5f5f5;
+                color: #666;
+                text-align: right;
+                padding: 10px;
+                font-size: 16px;
+            }
+        </style>
+        """
+
+def write_footer():
+    st.markdown(
+        """
+        <style>
+            .footer {
+                position: fixed;
+                left: 0;
+                bottom: 0;
+                width: 100%;
+                background-color: #f5f5f5;
+                color: #666;
+                text-align: right;
+                padding: 10px;
+                font-size: 16px;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+    st.markdown(
+        '<div class="footer"><a href="http://localhost:8501/impressum">Impressum</a> '
+        '| <a href="https://www.uni-bremen.de/en/data-privacy">Privacy Policy</a></div>',
+        unsafe_allow_html=True
+    )
