b93f1388819e446f6df0e2bb1e0860fd956796d9
unknown <rockstro@uni-potsdam.de>
2023-07-22 14:48:22 +0200

    caught exception resulting from different calendar

--- antonomasia/verbalizer.py
+++ antonomasia/verbalizer.py
@@ -2,16 +2,18 @@ from antonomasia.embeddings import KGE
 from wikidata.client import Client
 
 from antonomasia.utils import Sample
+from wikidata.datavalue import DatavalueError
+
 
 class Verbalizer(object):
-  def __init__(self):
-    """
+    def __init__(self):
+        """
     Initialize verbalizer
     """
-    self.client = Client()
+        self.client = Client()
 
-  def generate_sentence(self, a: Sample, b: Sample, c: str) -> str:
-    """
+    def generate_sentence(self, a: Sample, b: Sample, c: str) -> str:
+        """
     Generate a sentence starting from sample A, B and c context.
 
     Args:
@@ -22,13 +24,15 @@ class Verbalizer(object):
     Returns:
         str: Generated sentence. Take into account if A is dead.
     """
-    a_entity = self.client.get(a.wikidata_iri, load=True)
-    c_entity = self.client.get(c, load=True)
-    wikicommons_category = self.client.get("P373", load=True)
-    c_label = a_entity[c_entity][wikicommons_category].lower()
+        a_entity = self.client.get(a.wikidata_iri, load=True)
+        c_entity = self.client.get(c, load=True)
+        wikicommons_category = self.client.get("P373", load=True)
+        c_label = a_entity[c_entity][wikicommons_category].lower()
+        # catch exeption resutling from different calendar model
+        try:
+            is_dead_prop = self.client.get("P570", load=True)
+            verb = "was" if is_dead_prop in a_entity else "is"
+        except DatavalueError:
+            verb = "was"
 
-    is_dead_prop = self.client.get("P570", load=True)
-
-    verb = "was" if is_dead_prop in a_entity else "is"
-
-    return f"{a.label} {verb} the {b.label} of {c_label}"
+        return f"{a.label} {verb} the {b.label} of {c_label}"
