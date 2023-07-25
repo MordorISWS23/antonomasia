d9b8f47302f439655cb569592e3a88a89f791cb3
unknown <rockstro@uni-potsdam.de>
2023-07-25 14:56:29 +0200

    caught exception for missing commons category for c_label

--- antonomasia/verbalizer.py
+++ antonomasia/verbalizer.py
@@ -27,12 +27,16 @@ class Verbalizer(object):
         a_entity = self.client.get(a.wikidata_iri, load=True)
         c_entity = self.client.get(c, load=True)
         wikicommons_category = self.client.get("P373", load=True)
-        c_label = a_entity[c_entity][wikicommons_category].lower()
+        try:
+            c_label = a_entity[c_entity][wikicommons_category].lower()
+        except KeyError:
+            prof = a_entity[c_entity]
+            prof_label = prof.label
+            c_label = f"{str(prof_label).lower()}s"
         # catch exeption resutling from different calendar model
         try:
             is_dead_prop = self.client.get("P570", load=True)
             verb = "was" if is_dead_prop in a_entity else "is"
         except DatavalueError:
             verb = "was"
-
-        return f"{a.label} {verb} the {b.label} of {c_label}"
+        return f"{a.label} {verb} the {b.label} of {c_label}."
